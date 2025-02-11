596f0dc916c2c3eb944f6f63e52526f790a6b142
xmr88 <47277408+xmr88@users.noreply.github.com>
2025-02-11 20:06:36 +0200

    Delete FinalProject directory

--- FinalProject/chatappds/src/main/java/com/example/chatappds/repository/ChannelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.chatappds.repository;
-
-import com.example.chatappds.model.Channel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChannelRepository extends JpaRepository<Channel, Long> {
-}
