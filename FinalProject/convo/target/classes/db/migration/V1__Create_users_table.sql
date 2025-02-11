660e5ff5a4d901192a0878b45087f3ac4f6859cb
xmr88 <47277408+xmr88@users.noreply.github.com>
2025-02-11 19:23:22 +0200

    Delete FinalProject directory

--- FinalProject/convo/target/classes/db/migration/V1__Create_users_table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE users (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       username VARCHAR(50) NOT NULL UNIQUE
-);
--- FinalProject/convo/target/classes/db/migration/V4__Create_friends_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE friends (
-                         user_id INT NOT NULL,
-                         friend_id INT NOT NULL,
-                         PRIMARY KEY (user_id, friend_id),
-                         FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-                         FOREIGN KEY (friend_id) REFERENCES users(id) ON DELETE CASCADE
-);
